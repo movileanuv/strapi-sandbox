dfea81d7fc4dd7c43774a044ccac5c76a991d4e2
Victor <victor.movileanu@check24.de>
2023-05-24 17:35:49 +0200

    use graphql for the list view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,27 @@ import Homepage from './pages/Homepage'
 import ReviewDetails from './pages/ReviewDetails'
 import SiteHeader from './components/SiteHeader'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql/',
+  cache: new InMemoryCache()
+})
 
 function App() {
 
   return (
     <Router>
-      <div className='App'>
-        <SiteHeader></SiteHeader>
-        <Routes>
-          <Route exact path='/' element={<Homepage/>}></Route>
-          <Route path='/details/:id' element={<ReviewDetails/>}></Route>
-          <Route path='/category/:id' element={<Category/>}></Route>
-        </Routes>
-      </div>
+      <ApolloProvider client={client}>
+        <div className='App'>
+          <SiteHeader></SiteHeader>
+          <Routes>
+            <Route exact path='/' element={<Homepage/>}></Route>
+            <Route path='/details/:id' element={<ReviewDetails/>}></Route>
+            <Route path='/category/:id' element={<Category/>}></Route>
+          </Routes>
+        </div>
+      </ApolloProvider>
     </Router>
   )
 }
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,24 @@
+import { useQuery, gql } from '@apollo/client'
 import React from 'react'
-import useFetch from '../hooks/useFetch'
 import { Link } from 'react-router-dom'
 
+const REVIEWS = gql`
+query GetReviews {
+    reviews {
+      data {
+        id
+        attributes {
+          title
+          rating
+          body
+        }
+      }
+    }
+  }
+`
+
 export default function Homepage() {
-    const {loading, error, data} = useFetch('http://localhost:1337/api/reviews')
+    const {loading, error, data} = useQuery(REVIEWS)
 
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error :(</p>
@@ -11,7 +26,7 @@ export default function Homepage() {
     console.log(data)
     return (
     <div>
-        {data.map(review => (
+        {data.reviews.data.map(review => (
             <div key={review.id} className='review-card'>
                 <div className='rating'>{review.attributes.rating}</div>
                 <h2>{review.attributes.title}</h2>
